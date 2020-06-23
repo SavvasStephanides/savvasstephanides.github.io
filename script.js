7342a3a8857f9696dc8edad406848af3b747b098
SavvasStephanides <savvascyp@hotmail.com>
2020-06-23 08:41:07 +0100

    Redo

--- script.js
+++
@@ -1,7 +0,0 @@
-function slideProfiles(){
-    var container = document.querySelector('.profiles-container');
-
-    var mode = container.getAttribute("mode");
-
-    container.setAttribute("mode", mode === "open" ? "closed" : "open");
-}
